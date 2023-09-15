b11f5d32c8b26e86247169e5b174f89bed2ae29e
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2023-09-15 15:58:58 +0530

    first commit

---
+++ Loops.js
@@ -0,0 +1,72 @@
+// print 1 to 5
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// using for loop
+
+// for(let i = 1;i<=5;i++){
+//     console.log(i)
+// }
+// for(let i = 0;i<5;i++){
+//     console.log(i+1)
+// }
+
+// printing array element
+// let fruits = ["mango","orange","grapes","banana"];
+// for(let i = 0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+// customisation in for loop- for-in
+// 
+// for(let i in fruits){
+//     console.log(fruits[i]);
+// }
+
+// let person = {name:"Tiya",age:"10"}
+// for(let i in person){
+//     console.log(person[i])
+// }
+
+
+// let fruits = ["mango","orange","grapes","banana"];
+// for(i of fruits)
+// {
+//     console.log(i) //no need to write fruits[]
+// }
+
+// var names = "Tiya";
+// for(let i of names){
+//     console.log(i)
+// }
+
+// while loop
+// let i =1;
+// while(i<=10){
+//     console.log(i)
+//     i++;
+// }
+
+// let fruits = ["mango","orange","grapes","banana"];
+// let i = 0;
+// while(i<fruits.length){
+//     console.log(fruits[i])
+//     i++
+// }
+
+// do-while
+// let i =1;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10)
+
+let fruits = ["mango","orange","grapes","banana"];
+let i = 0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
